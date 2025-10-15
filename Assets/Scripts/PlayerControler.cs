541c6486c6ae0a863568d0cdbee216559f4b95d1
Evan Haba <hippoguy101@gmail.com>
2025-10-14 19:47:49 -0700

    adding

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -4,34 +4,117 @@ using UnityEngine.InputSystem;
 public class PlayerControler : MonoBehaviour
 {
     public InputActionReference Primary;
+    public InputActionReference Secondary;
     public InputActionReference Move;
     public float moveSpeed = 0.03f;
     public GameObject BulletPrefab;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject BeamPrefab;
+    public float primaryDelay = 0.25f;
+    
+    private bool primaryDown = false;
+    private float primaryNextTime = 0f;
+    private bool beamActive = false;
+    private bool beamCooldown = false;
+    private float beamTimer = 0f;
+    private Camera cam;
+
     void Start()
     {
-        
+        cam = Camera.main;
+        primaryDown = false;
+        primaryNextTime = 0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector2 move = Move.action.ReadValue<Vector2>();
         transform.position += new Vector3(move.x * moveSpeed, 0, move.y * moveSpeed);
+
+        Vector3 pos = cam.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = cam.ViewportToWorldPoint(pos);
+
+        if (primaryDown == true && Time.time >= primaryNextTime) {
+            float spawnDistance = 1f;
+
+            Vector3 spawnPos = transform.position + transform.forward * spawnDistance;
+
+            Instantiate(BulletPrefab, spawnPos, BulletPrefab.transform.rotation);
+
+            primaryNextTime = Time.time + primaryDelay;
+        }
+
+        if (beamActive)
+        {
+            beamTimer += Time.deltaTime;
+            if (beamTimer >= 2f)
+            {
+                StopBeam();
+                beamCooldown = true;
+                beamTimer = 0f;
+            }
+        }
+        else if (beamCooldown)
+        {
+            beamTimer += Time.deltaTime;
+            if (beamTimer >= 10f)
+            {
+                beamCooldown = false;
+                beamTimer = 0f;
+            }
+        }
     }
 
     private void OnEnable()
     {
-        Primary.action.started += OnPrimary;
+        Primary.action.performed += OnPrimaryDown;
+        Primary.action.canceled += OnPrimaryUp;
+        Secondary.action.started += OnBeamStart;
+        Secondary.action.canceled += OnBeamEnd;
     }
 
     private void OnDisable()
     {
-        Primary.action.started -= OnPrimary;
+        Primary.action.performed -= OnPrimaryDown;
+        Primary.action.canceled -= OnPrimaryUp;
+        Secondary.action.started -= OnBeamStart;
+        Secondary.action.canceled -= OnBeamEnd;
     }
 
-    private void OnPrimary(InputAction.CallbackContext context)
+    private void OnPrimaryDown(InputAction.CallbackContext context)
     {
-        Instantiate(BulletPrefab, transform.position, Quaternion.identity);
+        primaryDown = true;
+    }
+
+    private void OnPrimaryUp(InputAction.CallbackContext context)
+    {
+        primaryDown = false;
+    }
+
+    private void OnBeamStart(InputAction.CallbackContext context)
+    {
+        float spawnDistance = 1f;
+
+        Vector3 spawnPos = transform.position + transform.forward * spawnDistance;
+        if (beamCooldown || beamActive) return;
+        beamActive = true;
+        Quaternion BeamRotation = transform.rotation * Quaternion.Euler(90, 0, 0);
+        Instantiate(BeamPrefab, spawnPos, BeamRotation, transform);
+    }
+
+    private void OnBeamEnd(InputAction.CallbackContext context)
+    {
+        if (beamActive)
+            StopBeam();
+    }
+
+    private void StopBeam()
+    {
+        foreach (Transform child in transform)
+            if (child.CompareTag("Beam"))
+                Destroy(child.gameObject);
+
+        beamActive = false;
     }
 }
---
+++ Assets/Scripts/bulletMove.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class bulletMove : MonoBehaviour
+{
+    public float speed = 10f;
+    public float maxDistance = 30f;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if (Vector3.Distance(startPos, transform.position) > maxDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
